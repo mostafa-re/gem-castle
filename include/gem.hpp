8601873fee76e45b4dae45e8365617f542be8acb
Mostafa-RT <mostafa.rezazadeh@outlook.com>
2020-08-05 02:49:01 +0430

    create gem class header

---
+++ include/gem.hpp
@@ -0,0 +1,21 @@
+#ifndef GEM_HPP
+#define GEM_HPP
+
+#include <SFML/Graphics.hpp>
+
+namespace gc_game
+{
+   class Gem : public sf::Sprite
+   {
+   private:
+      sf::Texture sourceTex;
+      size_t point;
+
+   public:
+      Gem(const std::string &, size_t);
+      virtual bool runAnimation() = 0;
+      virtual ~Gem();
+   };
+} // namespace gc_game
+
+#endif // GEM_HPP
