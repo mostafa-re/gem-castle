7594d2d2dbcbff09a929c6964fce470706eaa37e
Mostafa-RT <mostafa.rezazadeh@outlook.com>
2020-08-05 02:16:52 +0430

    modify main.cpp to show a emty window for test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
-#include <iostream>
-
-using namespace std;
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-   cout << " -> hi there ..." << endl;
+   sf::RenderWindow window(sf::VideoMode(800, 600), "Test window", sf::Style::Close);
+   window.setFramerateLimit(60);
+   window.requestFocus();
+   sf::Event event;
+   while (window.isOpen())
+   {
+      while (window.pollEvent(event))
+      {
+         if (event.type == sf::Event::Closed)
+            window.close();
+      }
+      window.clear();
+
+      window.display();
+   }
 }
