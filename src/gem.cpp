a7e1f977cb3ca9499f0a4a84277e17f319278bb8
Mostafa-RT <mostafa.rezazadeh@outlook.com>
2020-08-05 03:41:29 +0430

    add namespace for gem code

--- src/gem.cpp
+++ src/gem.cpp
@@ -1,9 +1,10 @@
 #include "gem.hpp"
 
-using namespace gc_game;
-
-Gem::Gem(const std::string &path, size_t point) : point(point)
+namespace gc_game
 {
-   this->sourceTex.loadFromFile(path);
-   this->setTexture(this->sourceTex);
-}
+   Gem::Gem(const std::string &path, size_t point) : point(point)
+   {
+      this->sourceTex.loadFromFile(path);
+      this->setTexture(this->sourceTex);
+   }
+} // namespace gc_game
