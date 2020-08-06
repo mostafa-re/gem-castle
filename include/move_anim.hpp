d5e48df999e010d8f37cb45bdfe5e763e256c4d5
Mostafa-RT <mostafa.rezazadeh@outlook.com>
2020-08-06 12:31:27 +0430

    code and header edit of move_anim class

--- include/move_anim.hpp
+++ include/move_anim.hpp
@@ -12,7 +12,7 @@ namespace gc_game
 
    public:
       explicit MoveAnim(const std::chrono::milliseconds &, float);
-      virtual bool run(sf::Transformable &) override;
+      bool run(sf::Transformable &, const sf::Vector2f &);
    };
 } // namespace gc_game
 #endif //MOVE_ANIM_HPP
---
+++ src/move_anim.cpp
@@ -0,0 +1,29 @@
+#include "move_anim.hpp"
+
+namespace gc_game
+{
+   MoveAnim::MoveAnim(const std::chrono::milliseconds &stopDuration, float moveStep)
+       : AnimationBase(stopDuration), moveStep(moveStep) {}
+
+   bool MoveAnim::run(sf::Transformable &obj, const sf::Vector2f &endPos)
+   {
+      std::chrono::duration<double> duration = std::chrono::high_resolution_clock::now() - this->starTimePoint;
+      if (std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() > this->stopDuration.count())
+      {
+         if ((endPos - obj.getPosition()).x < 2.f || (endPos - obj.getPosition()).y < 2.f)
+         {
+            obj.setPosition(endPos);
+            return true;
+         }
+         else
+         {
+            obj.move((endPos - obj.getPosition()) * this->moveStep);
+            return false;
+         }
+      }
+      else
+      {
+         return false;
+      }
+   }
+} // namespace gc_game
