d43a907adb1f6e12a7f4711544c0aedaadbe7eb0
Mostafa-RT <mostafa.rezazadeh@outlook.com>
2020-08-19 19:41:53 +0430

    adding try-catch for main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,33 @@
 #include <string>
 #include <climits>
 #include <unistd.h>
+#include <iostream>
+#include <stdexcept>
 #include "game.hpp"
 
 int main()
 {
-   char hostname[HOST_NAME_MAX];
-   char username[LOGIN_NAME_MAX];
-   gethostname(hostname, HOST_NAME_MAX);
-   getlogin_r(username, LOGIN_NAME_MAX);
-   std::string playerName = std::string(username) + '@' + std::string(hostname);
-   gc_game::Game::getHandler().setPlayerName(playerName);
-   gc_game::Game::getHandler().run();
+   try
+   {
+      char hostname[HOST_NAME_MAX];
+      char username[LOGIN_NAME_MAX];
+      gethostname(hostname, HOST_NAME_MAX);
+      getlogin_r(username, LOGIN_NAME_MAX);
+      std::string playerName = std::string(username) + '@' + std::string(hostname);
+      gc_game::Game::getHandler().setPlayerName(playerName);
+      gc_game::Game::getHandler().run();
+   }
+   catch (std::exception &ex)
+   {
+      std::cerr << "-> Ooh, an fatal error leads the program to be terminated :(\n"
+                << "-> Here is the ERR_COMMET:\n"
+                << ex.what() << '\n';
+      exit(EXIT_FAILURE);
+   }
+   catch (...)
+   {
+      std::cerr << "-> Sorry, an UNKNOWN fatal error leads the program to be terminated :/\n"
+                << "-> No ERR_COMMET availbale!\n";
+      exit(EXIT_FAILURE);
+   }
 }
